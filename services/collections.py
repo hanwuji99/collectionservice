293204c178d01c0f8a1c6570def2fc0d8ce797d2
houpeng <hanwuji99@outlook.com>
2017-09-07 12:22:42 +0800

    修改缓存

--- services/collections.py
+++ services/collections.py
@@ -31,7 +31,7 @@ def add():
 def all():
     cs = Collections.all()
     # rediscache
-    # cs = Collections.cache_all()
+    cs = Collections.cache_all()
     cs = [i.json() for i in cs]
     return nice_json(cs)
 
